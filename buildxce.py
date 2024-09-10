2fefbaa72f67457251be42bb8832da688ac83829
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2024-09-10 17:41:02 +0200

    Redo CLI using click; add some functionality
    buildxce now has two subcommands: "create" to create a compute
    engine script directly on the host system, and "build" to build
    a container image. Both subcommands have options to run as a batch
    process or live xcube server, and to serve data dynamically or
    from pre-generated Zarrs.

--- buildxce.py
+++ buildxce.py
@@ -1,51 +1,135 @@
 #!/usr/bin/env python3
-
+import sys
 import tempfile
 import subprocess
-import argparse
+import logging
 import pathlib
 import textwrap
 from datetime import datetime
 
-import yaml
+import click
 import docker
 import nbformat
 from nbconvert import PythonExporter
 
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("notebook", type=str)
-    parser.add_argument(
-        "--workdir",
-        "-w",
-        type=str,
-        help="Use this as working directory (useful for debugging). "
-        "If omitted, use an automatically created temporary directory.",
-    )
-    parser.add_argument(
-        "--run",
-        "-r",
-        action="store_true",
-        help="Run the image after building it",
-    )
-    args = parser.parse_args()
-    if args.workdir:
-        convert(args.workdir, args)
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("notebook", type=str)
+#     parser.add_argument(
+#         "--workdir",
+#         "-w",
+#         type=str,
+#         help="Use this as working directory (useful for debugging). "
+#         "If omitted, use an automatically created temporary directory.",
+#     )
+#     parser.add_argument(
+#         "--run",
+#         "-r",
+#         action="store_true",
+#         help="Run the image after building it",
+#     )
+#     args = parser.parse_args()
+#     if args.workdir:
+#         convert(args.workdir, args)
+#     else:
+#         with tempfile.TemporaryDirectory() as temp_dir:
+#             convert(temp_dir, args)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command(help="Create a compute engine script on the host system")
+@click.option(
+    "-b",
+    "--batch",
+    is_flag=True,
+    help="Run as batch script after creating")
+@click.option(
+    "-s",
+    "--server",
+    is_flag=True,
+    help="Run as xcube server script after creating")
+@click.option(
+    "-f",
+    "--from-saved",
+    is_flag=True,
+    help="If batch and server both used, serve datasets from saved Zarrs")
+@click.argument(
+    "notebook",
+    type=click.Path()
+)
+@click.argument(
+    "output_dir",
+    type=click.Path()
+)
+def create(batch, server, from_saved, notebook, output_dir):
+    output_path = pathlib.Path(output_dir)
+    pathlib.Path.mkdir(output_path, parents=True, exist_ok=True)
+    write_script(output_path, notebook)
+    if batch or server:
+        args = ["python3", output_path / "execute.py"]
+        if batch:
+            args.append("--save")
+        if server:
+            args.append("--serve")
+        if from_saved:
+            args.append("--from-saved")
+        subprocess.run(args)
+
+
+@cli.command(help="Build a compute engine as a Docker image")
+@click.option(
+    "-b",
+    "--batch",
+    is_flag=True,
+    help="Run as batch script after creating")
+@click.option(
+    "-s",
+    "--server",
+    is_flag=True,
+    help="Run as xcube server script after creating")
+@click.option(
+    "-f",
+    "--from-saved",
+    is_flag=True,
+    help="If batch and server both used, serve datasets from saved Zarrs")
+@click.option(
+    "-w",
+    "--workdir",
+    type=str
+)
+@click.argument(
+    "notebook",
+    type=click.Path()
+)
+def build(batch, server, from_saved, workdir, notebook):
+    # TODO allow export of saved results from container
+    if workdir:
+        convert(workdir, notebook, batch or server)
     else:
         with tempfile.TemporaryDirectory() as temp_dir:
-            convert(temp_dir, args)
+            convert(temp_dir, notebook, batch or server)
 
 
-def convert(output_dir, args):
+def convert(output_dir, notebook, run):
     output_path = pathlib.Path(output_dir)
-    write_script(output_path, args.notebook)
+    write_script(output_path, notebook)
     export_conda_env(output_path)
     image = build_image(output_path)
-    if args.run:
+    if run:
         client = docker.from_env()
+        LOGGER.info(f"Running {image.tags}")
+        command = ["python", "execute.py"]
+        if run:
+            # TODO better mapping of CLI arguments
+            command += ["--save", "--serve", "--from-saved"]
         client.containers.run(
             image=image,
+            command=command,
             ports={8080: 8080},
             remove=True,
             #            auto_remove=True,
@@ -60,32 +144,12 @@ def write_script(output_dir, input_notebook):
     exporter = PythonExporter()
     (body, resources) = exporter.from_notebook_node(notebook)
 
-    snippet = textwrap.dedent(
-        """
-    import xarray as xr
-    import xcube
-    from xcube.server.server import Server
-    from xcube.server.framework import get_framework_class
-    import xcube.util.plugin
-    import xcube.core.new
+    with open(pathlib.Path(sys.argv[0]).parent / "wrapper.py", "r") as fh:
+        wrapper = fh.read()
 
-    xcube.util.plugin.init_plugins()
-    server = Server(
-        framework=get_framework_class("tornado")(),
-        config={}
-    )
-    context = server.ctx.get_api_ctx("datasets")
-
-    for name, thing in locals().copy().items():
-        if isinstance(thing, xr.Dataset) and not name.startswith("_"):
-            context.add_dataset(thing, name, style="bar")
-
-    server.start()
-    """
-    )
-    with open(pathlib.Path(output_dir) / "serve.py", "w") as fh:
+    with open(pathlib.Path(output_dir) / "execute.py", "w") as fh:
         fh.write(body)
-        fh.write(snippet)
+        fh.write(wrapper)
 
 
 def export_conda_env(output_path: pathlib.Path):
@@ -99,8 +163,8 @@ def build_image(docker_path):
     dockerfile = textwrap.dedent(
         """
     FROM quay.io/bcdev/xcube:v1.7.0
-    COPY serve.py serve.py
-    CMD python serve.py
+    COPY execute.py execute.py
+    CMD python execute.py
     """
     )
     with open(docker_path / "Dockerfile", "w") as fh:
@@ -111,6 +175,5 @@ def build_image(docker_path):
     )
     return image
 
-
 if __name__ == "__main__":
-    main()
+     cli()
