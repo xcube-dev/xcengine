a804737a0938520fdfbcd603ce3e61a678cc890e
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2024-11-21 14:15:48 +0100

    Tidy up some shebang lines and copyright notices

--- xcengine/core.py
+++ xcengine/core.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Copyright (c) 2024 by Brockmann Consult GmbH
 # Permissions are hereby granted under the terms of the MIT License:
 # https://opensource.org/licenses/MIT.
--- xcengine/wrapper.py
+++ xcengine/wrapper.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2024 by Brockmann Consult GmbH
+# Permissions are hereby granted under the terms of the MIT License:
+# https://opensource.org/licenses/MIT.
+
+
 import logging
 import pathlib
 
