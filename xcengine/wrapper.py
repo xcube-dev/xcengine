d18cd8064fa253f4aba52a64a4c65fe3384bab20
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2025-03-19 10:57:14 +0100

    Update copyright years

--- xcengine/wrapper.py
+++ xcengine/wrapper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2024 by Brockmann Consult GmbH
+# Copyright (c) 2024-2025 by Brockmann Consult GmbH
 # Permissions are hereby granted under the terms of the MIT License:
 # https://opensource.org/licenses/MIT.
 
