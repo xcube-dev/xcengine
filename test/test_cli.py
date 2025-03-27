3d7e1e6a3bf59c500d2b101308804ba921887fab
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2025-03-27 16:11:24 +0100

    Improve cli.make_script test coverage

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,5 +1,4 @@
 from unittest.mock import patch
-
 import pytest
 from click.testing import CliRunner
 
@@ -10,7 +9,12 @@ from xcengine.cli import cli
 @patch("xcengine.core.ScriptCreator.convert_notebook_to_script")
 @patch("subprocess.run")
 @pytest.mark.parametrize("verbose_arg", [[], ["--verbose"]])
-def test_make_script(run_mock, convert_mock, init_mock, tmp_path, verbose_arg):
+@pytest.mark.parametrize("batch_arg", [[], ["--batch"]])
+@pytest.mark.parametrize("server_arg", [[], ["--server"]])
+@pytest.mark.parametrize("from_saved_arg", [[], ["--from-saved"]])
+def test_make_script(run_mock, convert_mock, init_mock, tmp_path, verbose_arg, batch_arg, server_arg, from_saved_arg):
+    from xcengine.cli import logging
+    logging.getLogger().setLevel(logging.WARN)
     nb_path = tmp_path / "foo.ipynb"
     nb_path.touch()
     output_dir = tmp_path / "bar"
@@ -20,25 +24,21 @@ def test_make_script(run_mock, convert_mock, init_mock, tmp_path, verbose_arg):
         cli,
         verbose_arg +
         [
-            "make-script",
-            "--batch",
-            "--server",
-            "--from-saved",
+            "make-script"] +
+        batch_arg + server_arg + from_saved_arg +
+[
             str(nb_path),
             str(output_dir),
         ],
     )
     convert_mock.assert_called()
     init_mock.assert_called()
-    run_mock.assert_called_with(
-        [
-            "python3",
-            output_dir / "execute.py",
-            "--batch",
-            "--server",
-            "--from-saved",
-        ]
-    )
-    from xcengine.cli import logging
+    if batch_arg or server_arg:
+        run_mock.assert_called_with(
+            [
+                "python3",
+                output_dir / "execute.py" ] +
+            batch_arg + server_arg + from_saved_arg
+        )
     assert logging.getLogger().getEffectiveLevel() == (logging.DEBUG if "--verbose" in verbose_arg else logging.WARNING)
     assert result.exit_code == 0
